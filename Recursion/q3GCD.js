7fb8e862e9038dd756f47da720b3dc2f710f8033
Akshat Trivedi <60979611+akshat-triv@users.noreply.github.com>
2021-09-22 16:22:49 +0530

    chore: added explanation for q3 calculating gcd

--- Recursion/q3GCD.js
+++ Recursion/q3GCD.js
@@ -1,3 +1,9 @@
+// This is by using Eucliden algorithm for getting GCD of two numbers
+// gcd(48, 18)
+// Step.1. 48 / 18 = 2 and remainder 12
+// Step.2. 18 / 12 = 1 and remainder 6 (We don't stop untill remainder is 0)
+// Step.3. 12 / 6 = 2 and remainder 0. Here we stop as remainder is 0 and we have our GCD as 6
+
 function calcGCD(num1, num2) {
     if (num1 < num2) {
         const temp = num1;
